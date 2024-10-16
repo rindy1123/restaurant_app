95b19af6f2fdbe0fc1d25311edddb93ea91fbef2
rindy1123 <genryusinbaou@gmail.com>
2024-10-16 20:15:46 +0900

    chore: close handler and path together

--- src/server/handler.rs
+++ src/server/handler.rs
@@ -2,7 +2,8 @@ use crate::db::ConnectionPool;
 use axum::{
     extract::{Path, State},
     http::StatusCode,
-    Json,
+    routing::{delete, get, post},
+    Json, Router,
 };
 use serde::{Deserialize, Serialize};
 use std::borrow::Borrow;
@@ -41,108 +42,128 @@ impl From<&Row> for OrderItem {
     }
 }
 
-pub async fn get_order_items(
-    Path(table_id): Path<i32>,
-    State(pool): State<ConnectionPool>,
-) -> Result<Json<OrderItems>, StatusCode> {
-    let conn = pool.get().await.unwrap();
-    let rows = conn
-        .query(
-            r#"
+pub fn get_order_items() -> Router<ConnectionPool> {
+    async fn handler(
+        Path(table_id): Path<i32>,
+        State(pool): State<ConnectionPool>,
+    ) -> Result<Json<OrderItems>, StatusCode> {
+        let conn = pool.get().await.unwrap();
+        let rows = conn
+            .query(
+                r#"
                 SELECT toi.*, t.table_number, mi.name menu_item_name FROM table_order_items toi
                 INNER JOIN menu_items mi ON toi.menu_item_id = mi.id
                 INNER JOIN tables t ON toi.table_id = t.id
                 WHERE table_id = $1;
             "#,
-            &[&table_id],
-        )
-        .await
-        .map_err(|e| {
-            eprintln!("Failed to query table_order_items: {}", e);
-            StatusCode::INTERNAL_SERVER_ERROR
-        })?;
-    let items = rows.iter().map(|row| row.into()).collect();
-    Ok(Json(OrderItems { items }))
+                &[&table_id],
+            )
+            .await
+            .map_err(|e| {
+                eprintln!("Failed to query table_order_items: {}", e);
+                StatusCode::INTERNAL_SERVER_ERROR
+            })?;
+        let items = rows.iter().map(|row| row.into()).collect();
+        Ok(Json(OrderItems { items }))
+    }
+
+    Router::<ConnectionPool>::new().route("/tables/:table_id/order_items", get(handler))
 }
 
-pub async fn get_order_item(
-    Path((table_id, order_item_id)): Path<(i32, i32)>,
-    State(pool): State<ConnectionPool>,
-) -> Result<Json<OrderItem>, StatusCode> {
-    let conn = pool.get().await.unwrap();
-    let order_item: OrderItem = conn
-        .query_one(
-            r#"
+pub fn get_order_item() -> Router<ConnectionPool> {
+    async fn handler(
+        Path((table_id, order_item_id)): Path<(i32, i32)>,
+        State(pool): State<ConnectionPool>,
+    ) -> Result<Json<OrderItem>, StatusCode> {
+        let conn = pool.get().await.unwrap();
+        let order_item: OrderItem = conn
+            .query_one(
+                r#"
                 SELECT toi.*, t.table_number, mi.name menu_item_name FROM table_order_items toi
                 INNER JOIN menu_items mi ON toi.menu_item_id = mi.id
                 INNER JOIN tables t ON toi.table_id = t.id
                 WHERE table_id = $1 AND toi.id = $2;
             "#,
-            &[&table_id, &order_item_id],
-        )
-        .await
-        .map_err(|e| {
-            eprintln!("Failed to query table_order_items: {}", e);
-            StatusCode::NOT_FOUND
-        })?
-        .borrow()
-        .into();
-    Ok(Json(order_item))
+                &[&table_id, &order_item_id],
+            )
+            .await
+            .map_err(|e| {
+                eprintln!("Failed to query table_order_items: {}", e);
+                StatusCode::NOT_FOUND
+            })?
+            .borrow()
+            .into();
+        Ok(Json(order_item))
+    }
+
+    Router::<ConnectionPool>::new()
+        .route("/tables/:table_id/order_items/:order_item_id", get(handler))
 }
 
-pub async fn create_order(
-    Path(table_id): Path<i32>,
-    State(pool): State<ConnectionPool>,
-    Json(params): Json<OrderPostParams>,
-) -> Result<StatusCode, StatusCode> {
-    let value_placeholders = params
-        .menu_item_ids
-        .iter()
-        .enumerate()
-        .map(|(i, _)| format!("(${}, ${}, ${})", i * 3 + 1, i * 3 + 2, i * 3 + 3))
-        .into_iter()
-        .collect::<Vec<String>>()
-        .join(", ");
-    let insert_statement = format!(
-        r#"
+pub fn create_order() -> Router<ConnectionPool> {
+    async fn handler(
+        Path(table_id): Path<i32>,
+        State(pool): State<ConnectionPool>,
+        Json(params): Json<OrderPostParams>,
+    ) -> Result<StatusCode, StatusCode> {
+        let value_placeholders = params
+            .menu_item_ids
+            .iter()
+            .enumerate()
+            .map(|(i, _)| format!("(${}, ${}, ${})", i * 3 + 1, i * 3 + 2, i * 3 + 3))
+            .into_iter()
+            .collect::<Vec<String>>()
+            .join(", ");
+        let insert_statement = format!(
+            r#"
             INSERT INTO table_order_items (table_id, menu_item_id, prep_time_minutes) VALUES
             {};
         "#,
-        value_placeholders
-    );
-    let values = params
-        .menu_item_ids
-        .iter()
-        .flat_map(|id| vec![&table_id, id, &10]) // TODO: make prep_time_minutes random
-        .collect::<Vec<&i32>>();
-    let conn = pool.get().await.unwrap();
-    conn.execute_raw(&insert_statement, values)
-        .await
-        .map_err(|e| {
-            eprintln!("Failed to insert into table_order_items: {}", e);
-            StatusCode::INTERNAL_SERVER_ERROR
-        })?;
-    Ok(StatusCode::CREATED)
+            value_placeholders
+        );
+        let values = params
+            .menu_item_ids
+            .iter()
+            .flat_map(|id| vec![&table_id, id, &10]) // TODO: make prep_time_minutes random
+            .collect::<Vec<&i32>>();
+        let conn = pool.get().await.unwrap();
+        conn.execute_raw(&insert_statement, values)
+            .await
+            .map_err(|e| {
+                eprintln!("Failed to insert into table_order_items: {}", e);
+                StatusCode::INTERNAL_SERVER_ERROR
+            })?;
+        Ok(StatusCode::CREATED)
+    }
+
+    Router::<ConnectionPool>::new().route("/tables/:table_id/orders", post(handler))
 }
 
-pub async fn delete_order_item(
-    Path((table_id, order_item_id)): Path<(i32, i32)>,
-    State(pool): State<ConnectionPool>,
-) -> Result<StatusCode, StatusCode> {
-    let conn = pool.get().await.unwrap();
-    conn.query(
-        "SELECT 1 FROM table_order_items WHERE table_id = $1 AND id = $2",
-        &[&table_id, &order_item_id],
-    )
-    .await
-    .map_err(|_| StatusCode::NOT_FOUND)?;
-
-    let delete_statement = "DELETE FROM table_order_items WHERE table_id = $1 AND id = $2;";
-    conn.execute(delete_statement, &[&table_id, &order_item_id])
+pub fn delete_order_item() -> Router<ConnectionPool> {
+    async fn handler(
+        Path((table_id, order_item_id)): Path<(i32, i32)>,
+        State(pool): State<ConnectionPool>,
+    ) -> Result<StatusCode, StatusCode> {
+        let conn = pool.get().await.unwrap();
+        conn.query_one(
+            "SELECT 1 FROM table_order_items WHERE table_id = $1 AND id = $2",
+            &[&table_id, &order_item_id],
+        )
         .await
-        .map_err(|e| {
-            eprintln!("Failed to insert into table_order_items: {}", e);
-            StatusCode::INTERNAL_SERVER_ERROR
-        })?;
-    Ok(StatusCode::NO_CONTENT)
+        .map_err(|_| StatusCode::NOT_FOUND)?;
+
+        let delete_statement = "DELETE FROM table_order_items WHERE table_id = $1 AND id = $2;";
+        conn.execute(delete_statement, &[&table_id, &order_item_id])
+            .await
+            .map_err(|e| {
+                eprintln!("Failed to insert into table_order_items: {}", e);
+                StatusCode::INTERNAL_SERVER_ERROR
+            })?;
+        Ok(StatusCode::NO_CONTENT)
+    }
+
+    Router::<ConnectionPool>::new().route(
+        "/tables/:table_id/order_items/:order_item_id",
+        delete(handler),
+    )
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,26 +1,14 @@
 use crate::db::ConnectionPool;
-use axum::{
-    routing::{delete, get, post},
-    Router,
-};
+use axum::Router;
 
 mod handler;
 
 pub async fn run(pool: ConnectionPool) {
     let app = Router::new()
-        .route(
-            "/tables/:table_id/order_items",
-            get(handler::get_order_items),
-        )
-        .route(
-            "/tables/:table_id/order_items/:order_item_id",
-            get(handler::get_order_item),
-        )
-        .route("/tables/:table_id/orders", post(handler::create_order))
-        .route(
-            "/tables/:table_id/order_items/:order_item_id",
-            delete(handler::delete_order_item),
-        )
+        .merge(handler::get_order_items())
+        .merge(handler::get_order_item())
+        .merge(handler::create_order())
+        .merge(handler::delete_order_item())
         .with_state(pool);
     // TODO: Get an address from environment variables
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
